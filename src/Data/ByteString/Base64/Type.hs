32b6d2bcde89ea3b3f1da521d5f2b9a450df20a2
Oleg Grenrus <oleg.grenrus@iki.fi>
2015-10-23 11:59:19 +0300

    Add semigroup, nfdata and hashable instances

--- src/Data/ByteString/Base64/Type.hs
+++ src/Data/ByteString/Base64/Type.hs
@@ -1,7 +1,7 @@
-{-# LANGUAGE CPP #-}
+{-# LANGUAGE CPP                #-}
 {-# LANGUAGE DeriveDataTypeable #-}
-{-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE Trustworthy #-}
+{-# LANGUAGE DeriveGeneric      #-}
+{-# LANGUAGE Trustworthy        #-}
 ----------------------------------------------------------------------------
 -- |
 -- Module      :  Data.ByteString.Base64.Type
@@ -12,33 +12,39 @@
 --
 -- 'ByteString64' is a strict 'ByteString' serialised with base64 encoding.
  ----------------------------------------------------------------------------
-module Data.ByteString.Base64.Type (ByteString64(..), module Data.ByteString) where
+module Data.ByteString.Base64.Type (
+    ByteString64(..),
+    module Data.ByteString
+  ) where
 
 #if !MIN_VERSION_base(4, 8, 0)
 import Control.Applicative
-import Data.Monoid
 #endif
 
-import Data.Aeson
-import Data.Binary
+import Control.DeepSeq          (NFData)
+import Data.Aeson               (ToJSON(..), FromJSON(..), withText)
+import Data.Binary              (Binary)
 import Data.ByteString
-import Data.ByteString.Base64 as Base64
-import Data.Data
-import Data.Serialize
-import Data.String
-import Data.Text.Encoding (encodeUtf8, decodeUtf8With)
-import Data.Text.Encoding.Error
-import GHC.Generics
+import Data.ByteString.Base64   as Base64
+import Data.Data                (Data, Typeable)
+import Data.Hashable            (Hashable)
+import Data.Semigroup
+import Data.Serialize           (Serialize)
+import Data.String              (IsString (..))
+import Data.Text.Encoding       (decodeUtf8With, encodeUtf8)
+import Data.Text.Encoding.Error (ignore)
+import GHC.Generics             (Generic)
 
 -- | Aeson serialisable bytestring. Uses base64 encoding.
 newtype ByteString64 = ByteString64 { getByteString64 :: ByteString }
-  deriving (Eq, Show, Ord, Data, Typeable, Generic)
+    deriving (Eq, Show, Ord, Data, Typeable, Generic)
 
 instance ToJSON ByteString64 where
-  toJSON = toJSON . decodeUtf8With ignore  . Base64.encode . getByteString64
+    toJSON = toJSON . decodeUtf8With ignore  . Base64.encode . getByteString64
 
 instance FromJSON ByteString64 where
-  parseJSON v = ByteString64 <$> withText "ByteString" (pure . decodeLenient .  encodeUtf8) v
+    parseJSON = withText "ByteString" $
+        pure . ByteString64 . decodeLenient . encodeUtf8
 
 -- | 'ByteString64' is serialised as 'ByteString'
 instance Serialize ByteString64
@@ -47,9 +53,14 @@ instance Serialize ByteString64
 instance Binary ByteString64
 
 instance IsString ByteString64 where
-  fromString = ByteString64 . fromString
+   fromString = ByteString64 . fromString
+
+instance NFData ByteString64
+instance Hashable ByteString64
+
+instance Semigroup ByteString64 where
+    ByteString64 a <> ByteString64 b = ByteString64 (a <> b)
 
 instance Monoid ByteString64 where
-  mempty = ByteString64 mempty
-  mappend (ByteString64 a) (ByteString64 b) = ByteString64 (mappend a b)
-  mconcat = ByteString64 . mconcat . fmap getByteString64
+    mempty = ByteString64 mempty
+    mappend = (<>)
